1f8968ae1cbdf57e30f1a3ea91cdf28bfc5ecc7b
Rossprehn <32393462+Rossprehn@users.noreply.github.com>
2019-01-20 20:22:41 -0700

    display profiles

--- app/Controllers/Http/ProfileController.js
+++ app/Controllers/Http/ProfileController.js
@@ -5,19 +5,10 @@ const Profile = use('App/Models/Profile')
 class ProfileController {
     async home({view}) {
 
-        // Create a profile
-        const profile = new Profile;
-        profile.user_name = 'My profile name';
-        profile.email = 'rossprehn@me.com';
-        profile.location = 'this is my location';
-        profile.bio = 'this is my bio';
-
-        await profile.save();
-
-        // Fetch a profile
+        
         const profiles = await Profile.all();
 
-        return view.render('index', { profiles: profiles.toJSON() })
+        return view.render('index', { profiles:profiles.toJSON() })
     }
 }
 
